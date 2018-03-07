5f83234b93039e85f749dbacedabc2e44057202c
Katarzyna Zajaczewska <katarzyna.zajaczewska@oracle.com>
2018-03-07 15:19:01 +0100

    initial commit

---
+++ src/main/java/com/kz/incichecker/IncicheckerApplication.java
@@ -0,0 +1,12 @@
+package com.kz.incichecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncicheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncicheckerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kz/incichecker/IncicheckerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kz.incichecker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IncicheckerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
